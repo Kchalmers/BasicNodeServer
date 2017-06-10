6249eff384e397be33102a657c0a799bc515f5c1
Kchalmers <kevinqchal@yahoo.com>
2017-06-09 22:00:18 -0700

    added in express

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-const http = require('http');
-var fs = require('fs');
+var express = require('express');
+var app = express();
 
-http.createServer(function (req, res) {
+var port = process.env.PORT || 3000;
 
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    let obj = {
-        firstname: "kevin",
-        lastname: 'chalmers'
-    }
-    res.end(JSON.stringify(obj))
-}).listen(1337, '127.0.0.1');
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+app.use('/', function (req, res, next) {
+    console.log('Request Url:' + req.url);
+    next();
+});
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.get('/person/:id', function(req, res) {
+    res.render('person', { ID: req.params.id });
+});
+
+app.get('/api', function(req, res) {
+    res.json({ firstname: 'John', lastname: 'Doe' });
+});
+
+app.listen(port);
